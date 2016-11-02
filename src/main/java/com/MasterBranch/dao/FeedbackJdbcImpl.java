c04ad38c4440059dd026b672d416e4fb566c29c1
Kurki Teemu <a1503065@myy.haaga-helia.fi>
2016-11-02 09:14:28 +0200

    jdbcImpl getAllQueries updated

--- src/main/java/com/MasterBranch/dao/FeedbackJdbcImpl.java
+++ src/main/java/com/MasterBranch/dao/FeedbackJdbcImpl.java
@@ -2,6 +2,7 @@ package com.MasterBranch.dao;
 
 import java.util.List;
 
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -24,10 +25,17 @@ public class FeedbackJdbcImpl {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 	
-	public List<Query> getAllQuerys(){
+	public List<Query> getAllQueries(int inqueryId){
 		String sql = "SELECT queries FROM inquires WHERE id = ?";
+		Object[] parameter = new Object[] { inqueryId };
 		RowMapper<Query> queryMapper = new FeedbackQueryRowMapper();
-		List<Query> queries = jdbcTemplate.query(sql, queryMapper);
+		List<Query> queries = null;
+		try {
+			queries = jdbcTemplate.query(sql, parameter, queryMapper);
+		}catch(IncorrectResultSizeDataAccessException e){
+			System.out.println("Cannot find data from database");
+		}
 		return queries;
 	}
+	
 }
