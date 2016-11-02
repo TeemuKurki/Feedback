29df3ac54a8a5e896d1b4c2f0575e5b29ddc4231
Kurki Teemu <a1503065@myy.haaga-helia.fi>
2016-11-02 11:16:43 +0200

    Inquiry -> Enquiry

--- src/main/java/com/MasterBranch/dao/FeedbackEnquiryRowMapper.java
+++ src/main/java/com/MasterBranch/dao/FeedbackEnquiryRowMapper.java
@@ -5,13 +5,13 @@ import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
 
-import com.MasterBranch.bean.Inquiry;
+import com.MasterBranch.bean.Enquiry;
 
-public class FeedbackInquiryRowMapper implements RowMapper<Inquiry> {
+public class FeedbackEnquiryRowMapper implements RowMapper<Enquiry> {
 // Does not get queries
 	@Override
-	public Inquiry mapRow(ResultSet rs, int rowNum) throws SQLException {
-		Inquiry iq = new Inquiry();
+	public Enquiry mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Enquiry iq = new Enquiry();
 		iq.setDeployed(rs.getBoolean("isDeployed"));
 		iq.setId(rs.getInt("id"));
 		iq.setName(rs.getString("name"));
